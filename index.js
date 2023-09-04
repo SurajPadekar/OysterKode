3a5dd50ae1f3a50f464c881d6dc33cc31cf4ac71
SurajPadekar <144055735+SurajPadekar@users.noreply.github.com>
2023-09-04 23:38:10 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+<script>
+        document.getElementById('toggle-details').addEventListener('click', function () {
+            const detailsSection = document.querySelector('.details-section');
+            detailsSection.classList.toggle('in-view');
+        });
+    </script>
+
+
